1c7769573472703e558916000e6dddf3b2f81623
Jakub Hempel <jakubhempel@student.agh.edu.pl>
2023-10-26 19:02:00 +0200

    Home page

---
+++ 📃_Home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+# Customize page title
+st.title("Wieloczasowa analiza jakości wody w Odrze na podstawie zdjeć satelitarnych i Google Earth Engine")
+
+st.markdown(
+    """
+    This app presenting the way to controlling condition of water in Odra river from 2018 to present.
+    """
+)
