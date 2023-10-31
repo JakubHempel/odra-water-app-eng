3953c64d8838debf50ef432b95bfc01d40684f03
Jakub Hempel <100939247+JakubHempel@users.noreply.github.com>
2023-10-31 17:40:07 +0100

    Update 11_Ecological_Disaster_-_Oder_2022.py

--- pages/11_Ecological_Disaster_-_Oder_2022.py
+++ pages/11_Ecological_Disaster_-_Oder_2022.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.set_page_config(layout="wide", page_title="Disaster Odra 2022 | OdrApp 💦")
+st.set_page_config(layout="wide", page_title="Ecological Disaster - Oder 2022 | OdrApp 💦")
 
 import plotly.graph_objects as go
 import pandas as pd
