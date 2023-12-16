d96a7e8f3931eac9ac5267f7a8629c85be84a010
Jakub Hempel <jakubhempel@student.agh.edu.pl>
2023-12-16 12:51:47 +0100

    Add information about ecological disaster

--- pages/11_Ecological_Disaster_-_Oder_2022.py
+++ pages/11_Ecological_Disaster_-_Oder_2022.py
@@ -2,7 +2,21 @@ import streamlit as st
 
 st.set_page_config(page_title="Ecological Disaster - Oder 2022 | OdrApp 💦")
 
-st.subheader("Ecological Disaster - Oder 2022 ⚠️")
+st.markdown("""
+<style>
+.align-text {
+    text-align: justify;
+}
+</style>
+""", unsafe_allow_html=True)
+
+st.title("Ecological Disaster - Oder 2022 ⚠️")
+st.subheader("\n")
+
+st.markdown("""
+    <p class="align-text">
+    According to the Wstępny Raport Zespołu ds. sytuacji na rzece Odrze (Preliminary Report of the Team for the Situation on the Odra River), the disaster began on July 26, 2022, when dead fish were observed at the level of Olawa. Nevertheless, the phenomenon had been occurring much earlier, as early as mid-March. Initially, it was limited to the Lower Silesian Voivodeship, but quickly spread also to the Opolskie, Silesian, Lubuskie and West Pomeranian Voivodeships, as well as the Brandenburg area in Germany. Climatic changes and irrational water management in the basin area resulted in several weeks of drought, translated into record low water levels in the Odra River. Moreover, the concentration of chemical compounds introduced into the Odra River by industrial plants has reached high levels. The occurring drought and high air temperature led to an increase in water salinity, which was good conditions for the development of the microalgae Prymnesium parvum, the so-called golden algae. They have the ability to produce toxins (prymnesin), which are a threat to fish and other gill-breathing organisms. A report dated September 29, 2022, which was compiled on the research of 49 scientists (from a number of universities and state institutions such as, the General Directorate of Environmental Protection and the National Research Institute) indicated that about 250 tons of fish, beavers, mussels and birds were affected. The ecological disaster led to irreversible damage to the river's ecosystem.</p> 
+            """, unsafe_allow_html=True)
 
 st.divider()
 
@@ -14,5 +28,5 @@ st.markdown(
     - the Sections Chart 📈 page is still in progress 🙂.
 
     ##### Explore all 3 pages below this one on the sidebar! 💦
-    """
+    """ 
 )
